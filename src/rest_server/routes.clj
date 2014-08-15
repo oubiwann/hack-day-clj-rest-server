6f76eacd1b600e5f4e11273ea61426318ae82eeb
dzito <dan.zito@adroll.com>
2014-08-15 13:36:37 -0700

    Added routes to list orders, process payments

--- src/rest_server/routes.clj
+++ src/rest_server/routes.clj
@@ -2,6 +2,10 @@
 	(:require [compojure.core :refer :all]))
 
 (defroutes main-routes
+  (GET "/orders" [] "<h1>All Current Orders:</h1>")
   (POST "/order" [] "<h1>Order placed.</h1>")
   (GET "/order/:id" [id] (str "<h2>Order " id " is not ready.</h2>"))
-  (PUT "/order/:id" [id] (str "<h2>Received update for Order " id)))
+  (PUT "/order/:id" [id] (str "<h2>Received update for Order " id "</h2>"))
+  (DELETE "/order/:id" [id] (str "<h2>All done with Order " id "</h2>"))
+  (GET "/payment/order/:id" [id] (str "<h2>Payment Status</h2>"))
+  (PUT "/payment/order/:id" [id] (str "<h2>Paid for Order " id "</h2>")) )
