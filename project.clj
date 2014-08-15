a49e3b8f40d85d5302319c2715491e29187d1f80
Duncan McGreggor <duncan@cogitat.io>
2014-08-15 11:23:42 -0700

    Updated project with server stuff.

--- project.clj
+++ project.clj
@@ -3,5 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.8"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [http-kit "2.1.18"]
+                 [ring "1.3.0"]
+                 [compojure "1.1.8"]]
+  :source-paths ["src"]
+  :jvm-opts ^:replace ["-Xmx512m" "-server"]
+  :aot [rest-server.server]
+  :main rest-server.server/run)
