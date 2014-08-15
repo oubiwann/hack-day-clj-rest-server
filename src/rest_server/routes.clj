ecb0187f1ef504f602723a9750a06551db81eded
dzito <dan.zito@adroll.com>
2014-08-15 13:28:39 -0700

    stubbed route for PUT /order/:id

--- src/rest_server/routes.clj
+++ src/rest_server/routes.clj
@@ -3,4 +3,5 @@
 
 (defroutes main-routes
   (POST "/order" [] "<h1>Order placed.</h1>")
-  (GET "/order/:id" [id] (str "<h2>Order " id " is not ready.</h2>")))
+  (GET "/order/:id" [id] (str "<h2>Order " id " is not ready.</h2>"))
+  (PUT "/order/:id" [id] (str "<h2>Received update for Order " id)))
